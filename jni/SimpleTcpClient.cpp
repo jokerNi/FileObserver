fc955ad256df0bd44ee555680259e42b56279233
aaron <striversist@qq.com>
2014-01-24 17:39:39 +0800

    【功能】加入DNS解析逻辑

--- jni/SimpleTcpClient.cpp
+++ jni/SimpleTcpClient.cpp
@@ -1,10 +1,42 @@
 #include "SimpleTcpClient.h"
+#include <string>
 #include <strings.h>
 #include <stdio.h>
-#include <sys/socket.h>
+#include <netdb.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include "MyLog.h"
 
+using namespace std;
+
+int ResolveHost(const std::string& host, std::string& ip)
+{
+    std::string result;
+    struct addrinfo *answer, hint, *curr;
+    char ipstr[16] = {0};
+    bzero(&hint, sizeof(hint));
+    hint.ai_family = AF_INET;
+    hint.ai_socktype = SOCK_STREAM;
+
+    int ret = getaddrinfo(host.c_str(), NULL, &hint, &answer);
+    if (ret != 0) 
+    {
+        XLOG("ResolveHost getaddrinfo: %s\n", gai_strerror(ret));
+        return ret;
+    }
+
+    for (curr = answer; curr != NULL; curr = curr->ai_next) 
+    {
+        inet_ntop(AF_INET, &(((struct sockaddr_in*)(curr->ai_addr))->sin_addr), ipstr, 16);
+        XLOG("ResolveHost %s\n", ipstr);
+        ip = std::string(ipstr);
+        break;
+    }
+
+    freeaddrinfo(answer);
+    return 0;
+}
 
 SimpleTcpClient::SimpleTcpClient()
 {
@@ -20,9 +52,16 @@ SimpleTcpClient::SimpleTcpClient()
 
 int SimpleTcpClient::connect(const char* ip, int port)
 {
+    std::string resolvedIp;
+    if (ResolveHost(ip, resolvedIp) < 0)
+    {
+        XLOG("ResolveHost error");
+        return -1;
+    }
+    
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+    server_addr.sin_addr.s_addr = inet_addr(resolvedIp.c_str());
     server_addr.sin_port = htons(port);
     socklen_t server_addr_len = sizeof(server_addr);
     if (::connect(mClientSocket, (sockaddr*)&server_addr, server_addr_len) < 0)
